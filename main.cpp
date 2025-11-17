1a56fe09f5c84f68a0b588cf898e014ebbc87c29
lalkaman1981 <oleg.shtogrin2005@gmail.com>
2025-11-17 22:49:49 +0200

    change main.cpp

--- main.cpp
+++ main.cpp
@@ -3,35 +3,13 @@
 std::vector<coro_context *> manager;
 
 
-void func2(double a, double b, double* res) {
+void func2(double a, double b, int v, double* res) {
 
   std::cout << "Step 2" << std::endl;
   switch_context(manager[2], manager[1]);
   std::cout << "Step 4" << std::endl;
 
-  double c = a * b;
-  volatile double v1 = -1.0;
-  volatile double v2 = -1.0;
-  volatile double v3 = -1.0;
-  volatile double v4 = -1.0;
-  volatile double v5 = -1.0;
-  volatile double v6 = -1.0;
-  volatile double v7 = -1.0;
-  volatile double v8 = -1.0;
-  volatile double v9 = -1.0;
-  volatile double v10 = -1.0;
-  volatile double v11 = -1.0;
-  volatile double v12 = -1.0;
-  volatile double v13 = -1.0;
-  volatile double v14 = -1.0;
-  volatile double v15 = -1.0;
-  volatile double v16 = -1.0;
-  volatile double v17 = -1.0;
-  volatile double v18 = -1.0;
-  volatile double v19 = -1.0;
-  volatile double v20 = -1.0;
-  std::cout << v1 + v2 + v3 + v4 + v5 + v6 + v7 + v8 + v9 + v10
-                   + v11 + v12 + v13 + v14 + v15 + v16 + v17 + v18 + v19 + v20 << std::endl;
+  double c = a * b * (double) v;
   *res = c;
 
   switch_context(manager[2], manager[1]);
@@ -40,7 +18,7 @@ void func2(double a, double b, double* res) {
   (void)prevent_optimization;
 }
 
-void func1(double a, double b, double* res) {
+void func1(double a, double b, int v, double* res) {
 
   std::cout << "Step 1" << std::endl;
   switch_context(manager[1], manager[0]);
@@ -48,29 +26,7 @@ void func1(double a, double b, double* res) {
   switch_context(manager[1], manager[2]);
   std::cout << "Step 5" << std::endl;
 
-  double c = a + b;
-  volatile double v1 = 1.0;
-  volatile double v2 = 1.0;
-  volatile double v3 = 1.0;
-  volatile double v4 = 1.0;
-  volatile double v5 = 1.0;
-  volatile double v6 = 1.0;
-  volatile double v7 = 1.0;
-  volatile double v8 = 1.0;
-  volatile double v9 = 1.0;
-  volatile double v10 = 1.0;
-  volatile double v11 = 1.0;
-  volatile double v12 = 1.0;
-  volatile double v13 = 1.0;
-  volatile double v14 = 1.0;
-  volatile double v15 = 1.0;
-  volatile double v16 = 1.0;
-  volatile double v17 = 1.0;
-  volatile double v18 = 1.0;
-  volatile double v19 = 1.0;
-  volatile double v20 = 1.0;
-  std::cout << v1 + v2 + v3 + v4 + v5 + v6 + v7 + v8 + v9 + v10
-                   + v11 + v12 + v13 + v14 + v15 + v16 + v17 + v18 + v19 + v20 << std::endl;
+  double c = a + b + (double) v;
   *res = c;
 
   switch_context(manager[1], manager[0]);
@@ -93,9 +49,9 @@ int main() {
   double *res2 = new double;
   *res2 = 0;
 
-  call_coro(cr1, cr2, func1, 6.9, 3.14, res1);
+  call_coro(cr1, cr2, func1, 6.7, 3.14, 6, res1);
 
-  call_coro(cr1, cr3, func2, 4.20, 14.88, res2);
+  call_coro(cr1, cr3, func2, 4.2, 14.9, 7, res2);
 
   std::cout << *res1 << std::endl;
   std::cout << *res2 << std::endl;
