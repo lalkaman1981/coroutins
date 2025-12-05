afd44012cb0e90a2c94f35aa9f9ee4e52352d50b
Bohdanok <bohdan.milian@gmail.com>
2025-12-05 12:07:35 +0200

    minor style changes

--- test_mycomanager.cpp
+++ test_mycomanager.cpp
@@ -7,14 +7,11 @@
 #include "libmycotask/mycotask.h"
 
 void task_body(int a, int b, int& result) {
-    // result = a + b;
-    std::cout << "[Task4] Start  a=" << a << "  b=" << b << "\n";
+    std::cout << "[Task4] Start  a=" <<
+        a << "  b=" << b << "\n";
     mycotask::current_task()->yield();
 
     std::cout << "[Task4] Resume1\n";
-    mycotask::current_task()->yield();
-
-    std::cout << "[Task4] Resume2\n";
     mycotask::current_task()->yield();
 
     result = a + b;
@@ -23,28 +20,25 @@ void task_body(int a, int b, int& result) {
 int main() {
     auto task1 = mycotask::create_task([](){
         std::cout << "[Task1] Started" << std::endl;
-        mycotask::current_task()->yield(); // yield back to main
+        mycotask::current_task()->yield();
         std::cout << "[Task1] Resumed1" << std::endl;
-        mycotask::current_task()->yield(); // yield back to main
-        std::cout << "[Task1] Resumed2" << std::endl;
-        mycotask::current_task()->yield(); // yield back to main
-        std::cout << "[Task1] Resumed3" << std::endl;
+        mycotask::current_task()->yield();
+
     });
 
     auto task2 = mycotask::create_task([](){
         std::cout << "[Task2] Started" << std::endl;
-        mycotask::current_task()->yield(); // yield back to main
+        mycotask::current_task()->yield();
         std::cout << "[Task2] Resumed" << std::endl;
 
     });
 
     auto task3 = mycotask::create_task([](){
         std::cout << "[Task3] Started and Finished!" << std::endl;
-        // mycotask::current_task()->yield(); // yield back to main
-        // std::cout << "[Task3] Resumed" << std::endl;
     });
     int result = -69420;
-    auto arg_task = mycotask::create_task(task_body, 1, 2, std::ref(result));
+    auto arg_task = mycotask::create_task(task_body,
+                                        1, 2, std::ref(result));
 
     mycomanager manager{std::move(task1), std::move(task2)};
 
@@ -58,10 +52,4 @@ int main() {
 
     return 0;
 
-    // mycomanager mycomanager{std::move(task3)};
-    // mycomanager mycomanager{};
-    //
-    // mycomanager.append_task(std::move(task3));
-    //
-    // mycomanager.run();
 }
