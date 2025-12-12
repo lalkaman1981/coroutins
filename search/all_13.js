153eba41042cf7be84a33a46d46012aa2f75c474
gllekkoff <gllekkoff@users.noreply.github.com>
2025-12-12 20:13:00 +0000

    deploy: 2acb9a6492fa459eb3a29cd72283ad13338a3eed

--- search/all_13.js
+++ search/all_13.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['waiters_0',['waiters',['../classMyPromise.html#a72a2410c1e0a87c73709d6f54a6726cb',1,'MyPromise::waiters'],['../classMyPromise_3_01void_01_4.html#a2ede60bc28ee19eae52607fbbc838b14',1,'MyPromise&lt; void &gt;::waiters']]],
+  ['waiters_0',['waiters',['../classMyPromise.html#a8552cdd9e960d74e5c34c8ff9b08bd3d',1,'MyPromise::waiters'],['../classMyPromise_3_01void_01_4.html#ae41215e36c3a9026c817208d67ab93ea',1,'MyPromise&lt; void &gt;::waiters']]],
   ['waiting_20for_20the_20result_3a_20tt_20future_20get_20tt_1',['Waiting for the result: &lt;tt&gt;future.get()&lt;/tt&gt;',['../md_readme.html#autotoc_md12',1,'']]]
 ];
--- search/variables_d.js
+++ search/variables_d.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['waiters_0',['waiters',['../classMyPromise.html#a72a2410c1e0a87c73709d6f54a6726cb',1,'MyPromise::waiters'],['../classMyPromise_3_01void_01_4.html#a2ede60bc28ee19eae52607fbbc838b14',1,'MyPromise&lt; void &gt;::waiters']]]
+  ['waiters_0',['waiters',['../classMyPromise.html#a8552cdd9e960d74e5c34c8ff9b08bd3d',1,'MyPromise::waiters'],['../classMyPromise_3_01void_01_4.html#ae41215e36c3a9026c817208d67ab93ea',1,'MyPromise&lt; void &gt;::waiters']]]
 ];
